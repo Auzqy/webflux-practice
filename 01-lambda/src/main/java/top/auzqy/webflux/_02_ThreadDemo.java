9903a11e44342c9632f3278259d3b11c710149f7
au <auzhouqingyu@163.com>
2020-01-04 12:04:11 +0800

    add _02_ThreadDemo

--- 01-lambda/src/main/java/top/auzqy/webflux/_02_ThreadDemo.java
+++ 01-lambda/src/main/java/top/auzqy/webflux/_02_ThreadDemo.java
@@ -2,8 +2,12 @@ package top.auzqy.webflux;
 
 /**
  * description:  更好的方式是使用线程池
- *          简单示例一下 匿名函数 的使用
+ * 简单示例一下 匿名函数 的使用
+ *
+ *
+ *
  * createTime: 2020-01-04 11:52
+ *
  * @author au
  */
 public class _02_ThreadDemo {
@@ -21,9 +25,29 @@ public class _02_ThreadDemo {
         new Thread(() -> System.out.println("ok")).start();
     }
 
+    public boolean compare() {
+        Runnable runnable1 = () -> System.out.println("ok");
+        Runnable runnable2 = () -> System.out.println("ok");
+        return runnable1 == runnable2;
+    }
+
+    /**
+     * lambda 表达式就是返回了指定接口的对象实例
+     */
+    public void justSeeSee() {
+        Object runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("ok");
+            }
+        };
+        Object runnable1 = (Runnable)() -> System.out.println("ok");
+    }
+
     public static void main(String[] args) {
         _02_ThreadDemo threadDemo = new _02_ThreadDemo();
         threadDemo.beforeJdk8();
         threadDemo.afterJdk8();
+        System.out.println(threadDemo.compare());
     }
 }
